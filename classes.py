c9f19405f7d4b053c64bc912b52a8dd16cb929fc
Reo Yamanaka <ryamanaka807@gmail.com>
2020-12-23 13:02:17 -0500

    created employee and manager classes and started inquiry loops

---
+++ classes.py
@@ -0,0 +1,38 @@
+import random
+
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+
+
+    @property
+    def fullname(self):
+        return "{} {}".format(self.first, self.last)
+
+    @property
+    def email(self):
+        return "{}.{}{}@companyR.com".format(self.first, self.last, random.randint(0, 999))
+
+    def __repr__(self):
+        return "{}, {}, {}".format(self.first, self.last, self.pay)
+
+    def __str__(self):
+        return "{} - {}".format(self.fullname(), self.email)
+    
+class Manager(Employee):
+    def __init__(self, first, last, pay, officeId):
+        super().__init__(first, last, pay)
+        self.officeId = officeId
+
+class Developer(Employee):
+    def __init__(self, first, last, pay, progLang):
+        super().__init__(first, last, pay)
+        self.progLang = progLang
+
+class Intern(Employee):
+    def __init__(self, first, last, pay, school):
+        super().__init__(first, last, pay)
+        self.school = school
+        
---
+++ script.py
@@ -0,0 +1,113 @@
+from classes import Employee, Manager, Developer, Intern
+import sqlite3
+
+conn = sqlite3.connect("employee.db")
+c = conn.cursor()
+c.execute("""CREATE TABLE IF NOT EXISTS employees (
+            first TEXT,
+            last TEXT,
+            email TEXT,
+            pay INTEGER,
+            position TEXT,
+            detail TEXT
+            )""")
+
+def show(query):
+    listing = c.execute(query)
+    for element in listing:
+        fullname = element[0] + " " + element[1]
+        print("Name: %s" %fullname)
+        print("Email: %s" %element[2])
+        print("Pay: %d" %element[3])
+        print("Position: %s"%element[4])
+        print("Detail: %s\n"%element[5])
+
+def getInfo(position):
+    fname = input("Enter the {}'s first name: ".format(position))
+    lname = input("Enter the {}'s last name: ".format(position))
+    pay = int(input(("Enter the {}'s pay: ".format(position))))
+    return fname, lname, pay
+    
+def insert_emp(emp, position, detail):
+    with conn:
+        c.execute("INSERT INTO employees VALUES(:first, :last, :email, :pay, :position, :detail)", {'first':emp.first, 'last':emp.last, "email":emp.email, 'pay':emp.pay, "position":position, "detail":detail})
+
+
+
+
+while True:
+    print("\nWelcome to Company R")
+    print("\n---MAIN MENU---\n")
+    print("Choose an option:")
+    print("1 - View list of employees")
+    print("2 - Add an employee")
+    print("3 - Remove an employee")
+    option = input()
+    if option == "1":
+        while True:
+            print("---VIEWING MODE---") 
+            print("1 - See full list of employees")
+            print("2 - See full list of managers")
+            print("3 - See full list of developers")
+            print("4 - See full list of interns")
+            print("5 - Go back to main menu")
+            viewOption = input()
+            
+            if viewOption == "1":
+                print("Showing list of all employees...\n")
+                show("SELECT * FROM employees")
+            elif viewOption == "2":
+                print("Showing list of all managers...\n")
+                show("SELECT * FROM employees WHERE position = 'manager'")
+            elif viewOption == "3":
+                print("Showing list of all developers...\n")
+                show("SELECT * FROM employees WHERE position = 'developer'")
+            elif viewOption == "4":
+                print("Showing list of all interns...\n")
+                show("SELECT * FROM employees WHERE position = 'intern'")
+            elif viewOption == "5":
+                print("Returning to main menu...\n")
+                break
+            else:
+                print("Invalid option.")
+                
+                 
+    elif option == "2":
+        print("---ADDING MODE---")
+        print("1 - Add a manager")
+        print("2 - Add a developer")
+        print("3 - Add an intern")
+        print("4 - Go back to previous menu")
+        addOption = input()
+        while True:
+            if addOption == "1":
+                position = "manager"
+                fname, lname, pay = getInfo(position)
+                detail = int(input("Enter manager's office ID: "))
+                new_manager = Manager(fname, lname, pay, detail)
+                insert_emp(new_manager, position, "Office ID = {}".format(detail))
+                break
+
+            elif addOption == "2":
+                position = "developer"
+                detail = input("Enter the developer's primary programming language: ")
+            elif addOption == "3":
+                position = "intern"
+                detail = input("Enter the intern's school: ")
+            
+
+            
+            if addOption == "4":
+                print("Returning to previous menu...")
+                break
+        
+    elif option == "3":
+        print("---REMOVING MODE---")  
+        print("1 - Remove a manager")
+        print("2 - Remove a developer")
+        print("3 - Remove an intern")
+        print("4 - Go back to previous menu")
+        removeOption = input()
+        
+    else:
+        print("Invalid option.")
